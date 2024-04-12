583f325b5d0ec7bb6baf6f385b769608d32f1bea
Truong62 <truong06022004@gmail.com>
2024-04-12 13:36:43 +0700

    update

---
+++ Week-3/functions/addData.js
@@ -0,0 +1,32 @@
+const admin = require("firebase-admin")
+const serviceAccount = require("../serviceAccount.json")
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const faker = require('faker');
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+module.exports = db;
+
+function createRandomProduct() {
+    return {
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+    };
+}
+
+(async () => {
+    for (let i = 1; i <= 1000; i++) {
+        const product = createRandomProduct();
+        const res = await db.collection('Products').add(product);
+    }
+})()
+
+
+
+
